3668ae68a5f4a49ac56a2dcbc2bfebe20c689710
Felipe Belinassi <felipebelinassi@yahoo.com>
2018-01-24 02:18:38 -0200

    telas quase finalizadas

--- back-end/node/app/routes/login.js
+++ back-end/node/app/routes/login.js
@@ -1,6 +1,12 @@
+//retorna o token
+//let verifyToken = require('../middlewares/verifyToken');
+
 module.exports = function(app){
 	var api = app.api.login;
 
 	app.post('/v1/login', api.autentica);
+	//app.post('/v1/logout', api.logout);
+	//verifytoken usa o middleware
+	//app.use('/*', verifyToken ,api.verificatoken);
 	app.use('/*', api.verificatoken);
 }
