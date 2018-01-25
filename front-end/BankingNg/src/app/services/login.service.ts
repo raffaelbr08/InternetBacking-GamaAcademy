5e3260c5f031e29e5b77ca41d36e4381bfdef6be
rafael <rafael.hp8@gmail.com>
2018-01-25 12:04:40 -0200

    add

--- front-end/BankingNg/src/app/services/login.service.ts
+++ front-end/BankingNg/src/app/services/login.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {Http,Response,RequestOptions,Headers} from '@angular/http';
+import {Http, Response, RequestOptions, Headers} from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map'  ;
 
@@ -7,15 +7,16 @@ import 'rxjs/add/operator/map'  ;
 @Injectable()
 export class LoginService {
 
-  public login;
   public response = {
     nome:'',
     token:''
   }
-  
-  url = 'http://localhost:3000/v1/login'
+  public login;
 
-  constructor(public http: Http) { 
+
+  url = 'http://localhost:3000/v1/login/'
+
+  constructor(private http: Http) { 
 
   }
 
@@ -23,7 +24,7 @@ export class LoginService {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
 
-    let options = new RequestOptions({headers:headers})
+    let options = new RequestOptions({ headers: headers})
     return this.http.post(this.url,JSON.stringify(this.login),options);
   }
 
