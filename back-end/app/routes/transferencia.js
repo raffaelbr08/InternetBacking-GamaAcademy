a781ad4784f9a599406a6b27bf1bb48d6db6e751
evertonGil <egmsantos2@yahoo.com.br>
2018-01-25 16:10:38 -0200

    corrigindo rota extrato

--- back-end/app/routes/transferencia.js
+++ back-end/app/routes/transferencia.js
@@ -2,6 +2,6 @@
 module.exports = function(app){
     var api = app.api.transferencia;
     
-    app.post('/v1/transferencias', api.listaPorUsuario)
+    app.post('/v1/extrato', api.listaPorUsuario)
     app.post('/v1/transferencias', api.adiciona)
 }
