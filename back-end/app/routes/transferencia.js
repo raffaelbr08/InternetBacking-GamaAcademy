bc5df98444074b9eb592dbce0f2907e95bb51b9f
anderfilth <anderfilth@hotmail.com>
2018-01-25 16:07:11 -0200

    adicionar transferencia --parcial

--- back-end/app/routes/transferencia.js
+++ back-end/app/routes/transferencia.js
@@ -3,5 +3,5 @@ module.exports = function(app){
     var api = app.api.transferencia;
     
     app.post('/v1/transferencias', api.listaPorUsuario)
-    //app.post('/v1/transferencias', api.adiciona)
+    app.post('/v1/transferencias', api.adiciona)
 }
