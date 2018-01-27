1eb8b3becfa6944900758484d076e670ca231929
rafael <rafael.hp8@gmail.com>
2018-01-27 14:50:04 -0200

    ok

--- front-end/BankingNg/src/app/app-routing.module.ts
+++ front-end/BankingNg/src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
-import { ExtratoComponent } from './pages/extrato/extrato.component';
 import { TransfComponent } from './pages/transferencia/transf.component';
+import { ExtratoComponent } from './pages/extrato/extrato.component';
 
 const routes: Routes = [
 
@@ -10,17 +10,11 @@ const routes: Routes = [
     path:'', component:LoginComponent
   },
   {
-    path: 'extrato', component: ExtratoComponent
+    path: 'extrato',  component: ExtratoComponent
   },
   {
     path: 'transferencia', component: TransfComponent
-  },
-  {
-    path:'router', component: TransfComponent
-  }
-
- 
-
+  } 
 ];
 
 @NgModule({
