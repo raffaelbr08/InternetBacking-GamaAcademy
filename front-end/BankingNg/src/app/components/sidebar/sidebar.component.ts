d95501cee08e1089f4dcaf17ab00accf494770ea
fabianogma <fabianogma@gmail.com>
2018-01-28 00:51:36 -0200

    Sidebar: atualizacao de responsividade

--- front-end/BankingNg/src/app/components/sidebar/sidebar.component.ts
+++ front-end/BankingNg/src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,15 +8,37 @@ import { Component, OnInit } from '@angular/core';
 
 export class SidebarComponent implements OnInit {
 
-  private _opened: boolean = false;
+  private _opened = true;
+  private window_size = window.innerWidth;
+  private _mode = 'over';
 
   private _toggleSidebar() {
     this._opened = !this._opened;
+    // if (this.window_size < 800) {
+    //   this._opened = !this._opened;
+    // }
   }
 
   constructor() { }
 
   ngOnInit() {
+    if (this.window_size > 800) {
+      this._mode = 'push';
+    } else {
+      this._mode = 'over';
+      this._opened = false;
+    }
+   }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    if (event.target.innerWidth > 800) {
+      this._mode = 'push';
+      this._opened = true;
+    } else {
+      this._mode = 'over';
+      this._opened = false;
+    }
   }
 
 }
