c88576fbe9d7c5a4e696bbe425ac43b6fddd6e9d
unknown <egmsantos2@yahoo.com.br>
2018-01-29 18:23:35 -0200

    corrigindo scroll

--- front-end/BankingNg/src/app/app-routing.module.ts
+++ front-end/BankingNg/src/app/app-routing.module.ts
@@ -16,10 +16,13 @@ const routes: Routes = [
     path: 'extrato', component: ExtratoComponent, canActivate: [AuthGuard]
   }, 
   {
-    path:'**', component: Page404Component
+    path: 'transferencia', component: TransfComponent, canActivate: [AuthGuard]
   },
   {
     path: 'transferencia/:pagina', component: TransfComponent, canActivate: [AuthGuard]
+  },
+  {
+    path:'**', component: Page404Component
   }
 ];
 
