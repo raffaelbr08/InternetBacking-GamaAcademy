63c9ca6a2318bb9ff24bed49b9123779f778154f
Leticia Corral <le.martincorral@gmail.com>
2018-01-29 19:10:17 -0200

    mascara valor

--- front-end/BankingNg/src/app/pages/transferencia/outra-conta/outra-conta.component.ts
+++ front-end/BankingNg/src/app/pages/transferencia/outra-conta/outra-conta.component.ts
@@ -9,6 +9,7 @@ import { Router } from '@angular/router'
   templateUrl: './outra-conta.component.html',
   styleUrls: ['./outra-conta.component.scss']
 })
+
 export class OutraContaComponent implements OnInit {
   dadosUsuario = {}
 
@@ -49,6 +50,44 @@ export class OutraContaComponent implements OnInit {
       this.router.navigate(['/transferencia'])
     }
     
+  }
+
+  mascaraValor(e,m,r,a){
+
+    var code
+
+    // Cancela se o evento for Backspace
+    if (!e) e = window.event
+
+    if (e.keyCode) {
+      code = e.keyCode
+    }else if (e.which){
+      code = e.which
+    } 
+
+    var w = e.target
+
+    // Variáveis da função
+    var txt  = (!r) ? w.value.replace(/[^\d]+/gi,'') : w.value.replace(/[^\d]+/gi,'').split('').reverse().join('');
+    var mask = (!r) ? m : m.split('').reverse().join('');
+    var pre  = (a ) ? a.pre : "";
+    var pos  = (a ) ? a.pos : "";
+    var ret  = "";
+
+    if(code == 9 || code == 8 || txt.length == mask.replace(/[^#]+/g,'').length) return false;
+    
+    // Loop na máscara para aplicar os caracteres
+    for(var x=0,y=0, z=mask.length;x<z && y<txt.length;){
+      if(mask.charAt(x)!='#'){
+        ret += mask.charAt(x); x++; 
+      }else{
+        ret += txt.charAt(y); y++; x++; 
+      } 
+    }
+
+    // Retorno da função
+    ret = (!r) ? ret : ret.split('').reverse().join('')  
+    this.servicoTransf.dadosTransf.valor = pre+ret+pos; 
   }
 
 
